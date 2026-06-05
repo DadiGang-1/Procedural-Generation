765fa7d14af380e8a3d2a796fa6324c2597d063c
DadiGang-1 <david.world@hotmail.fr>
2026-06-05 16:30:28 +0200

    try procedural generation

--- src/com/david/perlin/MapNoiseGeneration.java
+++ src/com/david/perlin/MapNoiseGeneration.java
@@ -1,14 +1,15 @@
 package com.david.perlin;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class MapNoiseGeneration {
 	
 	private static final char[] STEP =  {' ', '░', '▒', '▓', '█'};
 	
-	private static final int WIDTH = 200;
-	private static final int LENGTH = 200;
-	private static final int FREQUENCY = 8;
+	private static final int WIDTH = 20;
+	private static final int LENGTH = 20;
+	private static final int FREQUENCY = 2;
 	
 	private static final int SEGMENT_LENGTH = WIDTH/FREQUENCY;
 	private static final int SEGMENT_NUMBER = WIDTH/SEGMENT_LENGTH;
@@ -20,15 +21,8 @@ public class MapNoiseGeneration {
 
 		float[][] randomPoint = new float[SEGMENT_NUMBER+1][SEGMENT_NUMBER+1];		
 		float[][] mapNoise = new float[WIDTH][LENGTH];
-		float[][] mapRandom = new float[WIDTH][LENGTH];
 				
 		Random random = new Random();
-		
-		for(int x = 0; x < WIDTH; x++) {
-			for(int y = 0; y < LENGTH; y++) {
-				mapRandom[x][y] = random.nextFloat();
-			}
-		}
 		
 		for(int x = 0; x < SEGMENT_NUMBER+1; x++) {
 			for(int y = 0; y < SEGMENT_NUMBER+1; y++) {
@@ -62,17 +56,74 @@ public class MapNoiseGeneration {
 			}
 		}
 		
-		
-		
-		
-		printMap(WIDTH, LENGTH, mapRandom);
-		System.out.println("\n");
-		printHeightMap(WIDTH, LENGTH, mapRandom);
-		System.out.println("\n");
 		printMap(SEGMENT_NUMBER+1, SEGMENT_NUMBER+1, randomPoint);
 		System.out.println("\n");
 		printHeightMap(WIDTH, LENGTH, mapNoise);
-
+		
+		
+		float[] lastRandomPoint = new float[SEGMENT_NUMBER+1];
+		for(int i = 0; i < SEGMENT_NUMBER; i++) {
+			lastRandomPoint[i] = randomPoint[SEGMENT_NUMBER][i];
+		}
+		
+		int loop = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			String userInput = scanner.nextLine();
+			if(userInput.equals("")) {
+				
+				float nextRandomPoint[] = new float[SEGMENT_LENGTH];
+				for(int i = 0; i < SEGMENT_NUMBER; i++) {
+					nextRandomPoint[i] =  random.nextFloat();
+				}
+				
+				float[][] mapGeneration = new float[SEGMENT_LENGTH][SEGMENT_LENGTH];
+				
+				for(int snX = 0; snX < SEGMENT_LENGTH; snX++) {			
+					for(int snY = 0; snY < SEGMENT_LENGTH; snY++) {	
+						
+						for(int x = 0; x < SEGMENT_LENGTH; x++) {
+							for(int y = 0; y < SEGMENT_LENGTH; y++) {
+								
+								float tx = x / ((SEGMENT_LENGTH-1)*1.0f);
+								float ty = y / ((SEGMENT_LENGTH-1)*1.0f);
+								
+								float a = lastRandomPoint[snX];
+								float b = lastRandomPoint[snX+1];
+								float c = nextRandomPoint[snX];
+								float d = nextRandomPoint[snX+1];
+								
+								float top = lerp(a, b, tx);
+								float bot = lerp(c, d, tx);
+								
+								float value = lerp(top, bot, ty);
+								
+								mapGeneration[x + (snX*SEGMENT_LENGTH)][y + (snY*SEGMENT_LENGTH)] = value;
+							}
+						}
+					}
+				}
+				
+				//for(int x = 0; x < SEGMENT_LENGTH; x++) {
+				//	float t = x / ((SEGMENT_LENGTH-1)*1.0f);
+				//	lineGeneration[x] = lerp(lastRandomPoint, nextRandomPoint, t);
+				//}
+				
+				lastRandomPoint = nextRandomPoint.clone();
+				
+				printHeightMap(SEGMENT_LENGTH, SEGMENT_LENGTH, mapGeneration);
+				loop++;
+			} else if(userInput.equals("0")) {
+				break;
+			}
+			
+		}
+		
+		
+		
+		
+		
 		
 	}
 	public static void printMap(int gridSizeX, int gridSizeY, float[][] map) {
